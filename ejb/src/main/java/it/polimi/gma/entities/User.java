341c1a215f056cfc9dc79c01a593220542af62d3
Luca Morandini <luca.morandini98@gmail.com>
2021-05-06 12:34:57 +0200

    Fixed User.getLeaderboard query

--- ejb/src/main/java/it/polimi/gma/entities/User.java
+++ ejb/src/main/java/it/polimi/gma/entities/User.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
         @NamedQuery(name = "User.getByUsername",
                 query = "SELECT u FROM User u WHERE u.username = :usr"),
         @NamedQuery(name = "User.getLeaderboard",
-                query = "SELECT DISTINCT u FROM User u LEFT JOIN Answer a ON a.user = u WHERE u.blocked = false AND a.questionnaire = :quest ORDER BY u.points DESC")
+                query = "SELECT DISTINCT u FROM User u JOIN Answer a ON a.user = u WHERE u.blocked = false AND a.questionnaire = :quest ORDER BY u.points DESC")
 })
 public class User implements Serializable {
     @Id
