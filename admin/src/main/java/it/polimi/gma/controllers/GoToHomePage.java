d5cc29ec15c5580d3a7190dcb811730dc496bfe8
Luca Morandini <luca.morandini98@gmail.com>
2021-05-14 19:42:49 +0200

    Added questionnaire of the day to admin page

--- admin/src/main/java/it/polimi/gma/controllers/GoToHomePage.java
+++ admin/src/main/java/it/polimi/gma/controllers/GoToHomePage.java
@@ -1,9 +1,11 @@
 package it.polimi.gma.controllers;
 
+import it.polimi.gma.services.QuestionnaireService;
 import it.polimi.gma.utils.ThymeleafFactory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.ejb.EJB;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -12,6 +14,9 @@ import java.io.IOException;
 @WebServlet(name = "GoToHomePage", value = "/Home")
 public class GoToHomePage extends HttpServlet {
     private TemplateEngine templateEngine;
+
+    @EJB(name = "QuestionnaireServiceEJB")
+    private QuestionnaireService questionnaireService;
 
     @Override
     public void init() throws ServletException {
@@ -22,6 +27,10 @@ public class GoToHomePage extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletContext servletContext = getServletContext();
         final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+        ctx.setVariable("questionnaire", questionnaireService.getQuestionnaireOfTheDay());
+        ctx.setVariable("statistical", questionnaireService.getStatisticalQuestions());
+
         templateEngine.process("/WEB-INF/Home.html", ctx, response.getWriter());
     }
 }
